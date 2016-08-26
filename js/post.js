667f2cc3dd0ce9ac3c7e0a18490f4d0212358b73
Alexandru Simonescu <alexandru.simonescu@gmail.com>
2016-08-26 20:52:29 +0200

    Fixed wrong config variable name

--- js/post.js
+++ js/post.js
@@ -139,7 +139,7 @@
       		          var dsq = document.createElement('script');
                     dsq.type = 'text/javascript'; dsq.async = true;
       		          dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-      		            (document.getElementsByTagName('head')[0] ||
+      		          (document.getElementsByTagName('head')[0] ||
                       document.getElementsByTagName('body')[0]).appendChild(dsq);
       			})();
       		}
